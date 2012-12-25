ee3e57e0e543f8aec288f65c703bffefd2a04cad
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2012-12-25 05:40:15 +0100

    Completely reworked

--- elevplandk-better-grades.user.js
+++ elevplandk-better-grades.user.js
@@ -11,109 +11,146 @@
 // ==/UserScript==
 document.addEventListener('DOMContentLoaded', function()
 {
+	function addStyle(css)
+	{
+		var head = document.getElementsByTagName('head')[0];
+		var style = document.createElement('style');
+		style.textContent = css;
+		style.type = 'text/css';
+		head.appendChild(style);
+	}
+	
+	var months = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
+	
+	function parseDate(date)
+	{
+		var words = date.split(' ');
+		return Number(words[1]) * 100 + Number(months.indexOf(words[0]));
+	}
+	
+	function round(number, decimals)
+	{
+		var a = Math.pow(10, decimals);
+		return Math.round(number * a) / a;
+	}
+	
+	var defaultWeight = 2;
+	var gradeWeight = {
+		'A': 2,
+		'B': 1.5,
+		'C': 1
+	};
+	
+	var dates = [];
+	var data = {};
 	var tables = document.querySelectorAll('table#fold_karakterer > tbody');
 	for(var t = 1; t < tables.length; t += 2)
 	{
-		var data = {};
-		
 		var rows = tables[t].querySelectorAll('tr.FormField');
-		var uniqueIds = [];
-		var uniqueRows = [];
+		
 		for(var i = 0; i < rows.length; i++)
 		{
 			var cells = rows[i].querySelectorAll('td');
 			var date = cells[0].textContent;
-			if(!data[date])
+			var subject = cells[1].textContent.replace(/(htx, )|(niveau )/g, '');
+			var type = cells[2].textContent.match('MDT')? 'Mundtlig': 'Skriftlig';
+			var grade = cells[3].textContent;
+			
+			if(!data[subject])
 			{
-				data[date] = {};
+				data[subject] = {};
 			}
-			var id = cells[1].textContent + '@' + cells[2].textContent;
-			data[date][id] = cells[3].textContent;
-			if(uniqueIds.indexOf(id) === -1)
+			
+			if(!data[subject][type])
 			{
-				uniqueIds.push(id);
-				uniqueRows.push(rows[i]);
+				data[subject][type] = {};
 			}
-			else
+			
+			data[subject][type][date] = grade;
+			
+			if(dates.indexOf(date) === -1)
 			{
-				tables[t].removeChild(rows[i]);
-			}
-		}
-		
-		var head = tables[t].querySelector('tr');
-		var months = 0;
-		
-		var averageRow = document.createElement('tr');
-		averageRow.classList.add('FormField');
-		
-		for(var i = 0; i < 2; i++)
-		{
-			var textCell = document.createElement('td');
-			textCell.classList.add('FormField');
-			textCell.appendChild(document.createTextNode(i === 0? 'Average': '-'));
-			averageRow.appendChild(textCell);
-		}
-		
-		for(var month in data)
-		{
-			if(data.hasOwnProperty(month))
-			{
-				var  grades = 0;
-				var sum = 0;
-				
-				var header = document.createElement('td');
-				header.classList.add('SearchResultHeader');
-				var text = document.createElement('h6');
-				text.appendChild(document.createTextNode(month));
-				header.appendChild(text);
-				head.insertBefore(header, head.querySelector('td:nth-of-type(4)'));
-				
-				for(var i = 0; i < uniqueRows.length; i++)
-				{
-					if(months === 0)
-					{
-						uniqueRows[i].removeChild(uniqueRows[i].querySelector('td'));
-						uniqueRows[i].removeChild(uniqueRows[i].querySelector('td:last-of-type'));
-					}
-					var cells = uniqueRows[i].querySelectorAll('td');
-					var id = cells[0].textContent + '@' + cells[1].textContent;
-					var cell = document.createElement('td');
-					cell.classList.add('FormField');
-					cell.setAttribute('align', 'center');
-					var grade = data[month][id];
-					if(grade)
-					{
-						cell.appendChild(document.createTextNode(grade));
-						grades++;
-						sum += Number(grade);
-					}
-					else
-					{
-						cell.appendChild(document.createTextNode('-'));
-					}
-					uniqueRows[i].insertBefore(cell, cells[2]);
-				}
-				
-				var averageCell = document.createElement('td');
-				averageCell.classList.add('FormField');
-				averageCell.setAttribute('align', 'center');
-				averageCell.appendChild(document.createTextNode(sum / grades));
-				averageRow.insertBefore(averageCell, averageRow.querySelector('td:nth-of-type(3)'));
-				
-				months++;
+				dates.push(date);
 			}
 		}
-		
-		tables[t].appendChild(averageRow);
-		
-		head.removeChild(head.querySelector('td'));
-		head.removeChild(head.querySelector('td:last-of-type'));
-		
-		tables[t - 1].querySelector('td').setAttribute('colspan', months + 2);
 	}
 	
-	if(typeof window.unsafeWindow !== 'undefined')
+	// Makes the grades go from old to new
+	dates.sort(function(a, b)
 	{
-		window.unsafeWindow.ShowAll(true);
+		var time1 = parseDate(a);
+		var time2 = parseDate(b);
+		return time1 - time2;
+	});
+	
+	var grades = {};
+	
+	var thead = '<thead><tr><th>Fag</th><th>Type</th>';
+	for(var i = 0; i < dates.length; i++)
+	{
+		thead += '<th>' + dates[i] + '</th>';
+		
+		grades[dates[i]] = {
+			sum: 0,
+			count: 0
+		};
 	}
+	thead += '</tr></thead>';
+	
+	var tbody = '<tbody>';
+	
+	for(var subject in data)
+	{
+		var types = Object.keys(data[subject]).length;
+		var totalWeight = (gradeWeight[subject.match(/.$/)] || defaultWeight);
+		var weight = totalWeight / types;
+		
+		tbody += '<tr><td rowspan="' + types + '">' + subject + '</td>';
+		firstRow = true;
+		for(var type in data[subject])
+		{
+			if(!firstRow)
+			{
+				tbody += '<tr>';
+			}
+			tbody += '<td>' + type + '</td>';
+			for(var i = 0; i < dates.length; i++)
+			{
+				var grade = data[subject][type][dates[i]];
+				if(grade)
+				{
+					grades[dates[i]].sum += Number(grade) * weight;
+					grades[dates[i]].count += weight;
+				}
+				else
+				{
+					grade = '-';
+				}
+				tbody += '<td>' + grade + '</td>';
+			}
+			firstRow = false;
+			tbody += '</tr>';
+		}
+	}
+	
+	tbody += '</tbody>';
+	
+	var tfoot = '<tfoot><tr><td>Gennemsnit</td><td> - </td>';
+	
+	for(var i = 0; i < dates.length; i++)
+	{
+		var average = grades[dates[i]].sum / grades[dates[i]].count;
+		tfoot += '<td>' + round(average, 2) + '</td>';
+	}
+	
+	tfoot += '</tr></tfoot>';
+	
+	var table = '<table>' + thead + tfoot + tbody + '</table>';
+	var div = document.createElement('div');
+	div.setAttribute('id', 'bettergrades');
+	div.innerHTML = table;
+	document.body.insertBefore(div, document.body.firstChild);
+	
+	addStyle("#bettergrades table { margin: auto; margin-top: 50px; margin-bottom: 50px; border-collapse: collapse; } #bettergrades thead, #bettergrades tfoot { background-color: lightgray; } #bettergrades th, #bettergrades td { padding: 0.5em; border: 1px solid; }");
+	
 });
