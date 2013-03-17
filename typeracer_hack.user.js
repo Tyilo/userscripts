a5d01ee55641edc4a3435a66f3b5ff28eb5cf9e1
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2013-03-17 13:54:04 +0100

    Create typeracer_hack.user.js

---
+++ typeracer_hack.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @id             typeracer_hack@tyilo.com
+// @name           Typeracer Hack
+// @version        1.0
+// @namespace      http://tyilo.com/
+// @author         Asger Drewsen <asgerdrewsen@gmail.com>
+// @description    Press space to win!
+// @include        http://play.typeracer.com/*
+// @run-at         document-start
+// @updateURL      https://raw.github.com/Tyilo/userscripts/master/typerace_hack.user.js
+// ==/UserScript==
+
+function loadScript(url, callback)
+{
+	// adding the script tag to the head as suggested before
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = url;
+	
+	// then bind the event to the callback function 
+	// there are several events for cross browser compatibility
+	script.onreadystatechange = callback;
+	script.onload = callback;
+	
+	// fire the loading
+	head.appendChild(script);
+}
+
+function setText(el) {
+	el.value = document.querySelector('*[id^="nhwMiddlegwt"]').textContent + document.querySelector('*[id^="nhwMiddleCommagwt"]').textContent;
+}
+
+function init() {
+	jQuery('body').on('keydown', 'input.txtInput', function() {
+		if(this.value === '') {
+			setText(this);
+		}
+	});
+}
+
+loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js', function() {
+	jQuery(init)
+});
