2bb996a22a114fd73191f754397fd88afb45b898
Asger Drewsen <asgerdrewsen@gmail.com>
2013-05-14 20:47:17 +0200

    Split tables into types and include newest grades column

--- elevplandk-better-grades.user.js
+++ elevplandk-better-grades.user.js
@@ -11,13 +11,40 @@
 // ==/UserScript==
 document.addEventListener('DOMContentLoaded', function()
 {
-	function addStyle(css)
+	function each(object, callback) {
+		for(var key in object) {
+			if(object.hasOwnProperty(key)) {
+				callback(key, object[key]);
+			}
+		}
+	};
+	
+	function clone(object, deep) {
+		var newObject = {};
+		each(object, function(key, value) {
+			if(deep && Object.prototype.toString.call(value) !== '[object Array]' && value instanceof Object) {
+				newObject[key] = value.clone(true);
+			} else {
+				newObject[key] = value;
+			}
+		});
+		return newObject;
+	};
+	
+	var addStyle = window.GM_addStyle || function addStyle(css)
 	{
 		var head = document.getElementsByTagName('head')[0];
 		var style = document.createElement('style');
 		style.textContent = css;
 		style.type = 'text/css';
 		head.appendChild(style);
+	};
+	
+	function oldestToNewest(a, b)
+	{
+		var time1 = parseDate(a);
+		var time2 = parseDate(b);
+		return time1 - time2;
 	}
 	
 	var months = ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'];
@@ -26,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function()
 	{
 		var words = date.split(' ');
 		return Number(words[1]) * 100 + Number(months.indexOf(words[0]));
-	}
+	}	
 	
 	function round(number, decimals)
 	{
@@ -45,8 +72,15 @@ document.addEventListener('DOMContentLoaded', function()
 		'SKR': 'Skriftlig',
 		'SAM': 'Sammenlagt'
 	};
+	var tableTypes = ['EKS', 'STA', 'GST', 'IPR'];
+	var tableNames = {
+		'EKS': 'Eksamen',
+		'GST': 'Gennemsnit',
+		'IPR': 'Intern prøve',
+		'STA': 'Standpunkt'
+	};
 	
-	var dates = [];
+	var dates = {};
 	var data = {};
 	var tables = document.querySelectorAll('table#fold_karakterer > tbody');
 	for(var t = 1; t < tables.length; t += 2)
@@ -58,132 +92,166 @@ document.addEventListener('DOMContentLoaded', function()
 			var cells = rows[i].querySelectorAll('td');
 			var date = cells[0].textContent;
 			var subject = cells[1].textContent.replace(/(htx, )|(niveau )/g, '');
-			var typeContent = cells[2].textContent;
-			var type = '-';
-			for(key in types) {
-				if(typeContent.match(key)) {
-					type = types[key];
-				}
-			}
-			//var type = cells[2].textContent.match(/^MDT/)? 'Mundtlig':  'Skriftlig';
+			
+			// \u00a0 == nbsp;
+			var typeValues = /([^,]*),\u00a0([^,]*),\u00a0([^,]*)/g.exec(cells[2].textContent);
+			var shortType = typeValues[1];
+			var type = types[shortType] || shortType;
+			var tableType = typeValues[2];
+			
 			var grade = cells[3].textContent;
 			
-			if(!data[subject])
-			{
-				data[subject] = {};
-			}
+			data[tableType] = data[tableType] || {};
+			data[tableType][subject] = data[tableType][subject] || {};
+			data[tableType][subject][type] = data[tableType][subject][type] || {};
 			
-			if(!data[subject][type])
-			{
-				data[subject][type] = {};
-			}
+			data[tableType][subject][type][date] = grade;
 			
-			data[subject][type][date] = grade;
-			
-			if(dates.indexOf(date) === -1)
+			dates[tableType] = dates[tableType] || [];
+			if(dates[tableType].indexOf(date) === -1)
 			{
-				dates.push(date);
+				dates[tableType].push(date);
 			}
 		}
 	}
 	
-	// Makes the grades go from old to new
-	dates.sort(function(a, b)
-	{
-		var time1 = parseDate(a);
-		var time2 = parseDate(b);
-		return time1 - time2;
-	});
+	// First child in body, before inserting tables
+	var firstChild = document.body.firstChild;
 	
 	var grades = {};
 	
-	var thead = '<thead><tr><th>Fag</th><th>Type</th>';
-	for(var i = 0; i < dates.length; i++)
+	for(var tableIndex = 0; tableIndex < tableTypes.length; tableIndex++)
 	{
-		thead += '<th>' + dates[i] + '</th>';
+		var tableType = tableTypes[tableIndex];
+		if(Object.keys(dates).indexOf(tableType) === -1) {
+			continue;
+		}
 		
-		grades[dates[i]] = {
+		var content = '<h1>' + tableNames[tableType] + '</h1>';
+		
+		var tableDates = dates[tableType];
+		// Makes the grades go from old to new
+		tableDates.sort(oldestToNewest);
+		
+		grades[tableType] = {};
+		var tableGrades = grades[tableType];
+		
+		var newestGrades = {};
+		
+		var thead = '<thead><tr><th>Fag</th><th>Type</th>';
+		
+		var noGrades = {
 			sum: 0,
 			count: 0,
-            weightedSum: 0,
-            weightedCount: 0
+			weightedSum: 0,
+			weightedCount: 0
 		};
-	}
-	thead += '</tr></thead>';
-	
-	var tbody = '<tbody>';
-	
-	for(var subject in data)
-	{
-		var types = Object.keys(data[subject]).length;
-		var totalWeight = (gradeWeight[subject.match(/.$/)] || defaultWeight);
-		var weight = totalWeight / types;
 		
-		tbody += '<tr><td rowspan="' + types + '">' + subject + '</td>';
-		firstRow = true;
-		for(var type in data[subject])
+		for(var i = 0; i < tableDates.length; i++)
 		{
-			if(!firstRow)
+			thead += '<th>' + tableDates[i] + '</th>';
+			
+			tableGrades[tableDates[i]] = clone(noGrades);
+		}
+		thead += '<th>Nyeste</th>';
+		thead += '</tr></thead>';
+	
+		var tbody = '<tbody>';
+		
+		var newestGradeCount = clone(noGrades);
+		var tableData = data[tableType];
+		for(var subject in tableData)
+		{
+			var types = Object.keys(tableData[subject]).length;
+			var totalWeight = (gradeWeight[subject.match(/.$/)] || defaultWeight);
+			var weight = totalWeight / types;
+			
+			tbody += '<tr><td rowspan="' + types + '">' + subject + '</td>';
+			var firstRow = true;
+			for(var type in tableData[subject])
 			{
-				tbody += '<tr>';
-			}
-			tbody += '<td>' + type + '</td>';
-			for(var i = 0; i < dates.length; i++)
-			{
-				var grade = data[subject][type][dates[i]];
-				if(grade)
+				if(!firstRow)
 				{
-                    grades[dates[i]].sum += Number(grade);
-                    grades[dates[i]].count += 1;
-					grades[dates[i]].weightedSum += Number(grade) * weight;
-					grades[dates[i]].weightedCount += weight;
+					tbody += '<tr>';
+				}
+				tbody += '<td>' + type + '</td>';
+				var newestGrade;
+				var newestGradeValue;
+				for(var i = 0; i < tableDates.length; i++)
+				{
+					var grade = tableData[subject][type][tableDates[i]];
+					if(grade)
+					{
+						tableGrades[tableDates[i]].sum += Number(grade);
+						tableGrades[tableDates[i]].count += 1;
+						tableGrades[tableDates[i]].weightedSum += Number(grade) * weight;
+						tableGrades[tableDates[i]].weightedCount += weight;
+						
+						newestGrade = grade;
+						newestGradeValue = tableGrades[tableDates[i]];
+					}
+					else
+					{
+						grade = '-';
+					}
+					tbody += '<td>' + grade + '</td>';
+				}
+				if(newestGrade)
+				{
+					for(var key in newestGradeValue) {
+						newestGradeCount[key] += newestGradeValue[key];
+					}
 				}
 				else
 				{
-					grade = '-';
+					newestGrade = '-';
 				}
-				tbody += '<td>' + grade + '</td>';
+				tbody += '<td>' + newestGrade + '</td>';
+				firstRow = false;
+				tbody += '</tr>';
 			}
-			firstRow = false;
-			tbody += '</tr>';
 		}
+		
+		tbody += '</tbody>';
+		
+		var values = [
+		{
+			'title': 'Gennemsnit',
+			'sumKey': 'sum',
+			'countKey': 'count'
+		},
+		{
+			'title': 'Vægted Gennemsnit',
+			'sumKey': 'weightedSum',
+			'countKey': 'weightedCount'
+		}];
+		
+		tableGrades.newest = newestGradeCount;
+		tableDates.push('newest');
+		
+		var tfoot = '<tfoot>';
+		for(var n = 0; n < 2; n++)
+		{
+			tfoot += '<tr><td>' + values[n].title + '</td><td> - </td>';
+			
+			for(var i = 0; i < tableDates.length; i++)
+			{
+				var average = tableGrades[tableDates[i]][values[n].sumKey] / tableGrades[tableDates[i]][values[n].countKey];
+				tfoot += '<td>' + round(average, 2) + '</td>';
+			}
+			
+			tfoot += '</tr>';
+		}
+		tfoot += '</tfoot>';
+		
+		content += '<table>' + thead + tfoot + tbody + '</table>';
+		var div = document.createElement('div');
+		div.setAttribute('id', 'bettergrades');
+		div.innerHTML = content;
+		document.body.insertBefore(div, firstChild);
 	}
 	
-	tbody += '</tbody>';
+	window.data = data;
 	
-    var values = [
-        {
-            'title': 'Gennemsnit',
-            'sumKey': 'sum',
-            'countKey': 'count'
-        },
-        {
-            'title': 'Vægted Gennemsnit',
-            'sumKey': 'weightedSum',
-            'countKey': 'weightedCount'
-        }];
-    
-    var tfoot = '<tfoot>';
-    for(var n = 0; n < 2; n++)
-    {
-        tfoot += '<tr><td>' + values[n].title + '</td><td> - </td>';
-        
-        for(var i = 0; i < dates.length; i++)
-        {
-            var average = grades[dates[i]][values[n].sumKey] / grades[dates[i]][values[n].countKey];
-            tfoot += '<td>' + round(average, 2) + '</td>';
-        }
-        
-        tfoot += '</tr>';
-    }
-    tfoot += '</tfoot>';
-	
-	var table = '<table>' + thead + tfoot + tbody + '</table>';
-	var div = document.createElement('div');
-	div.setAttribute('id', 'bettergrades');
-	div.innerHTML = table;
-	document.body.insertBefore(div, document.body.firstChild);
-	
-	addStyle("#bettergrades table { margin: auto; margin-top: 50px; margin-bottom: 50px; border-collapse: collapse; } #bettergrades thead, #bettergrades tfoot { background-color: lightgray; } #bettergrades th, #bettergrades td { padding: 0.5em; border: 1px solid; }");
-	
+	addStyle("#bettergrades { margin-top: 3em; } #bettergrades table { margin: auto; margin-top: 50px; margin-bottom: 50px; border-collapse: collapse; } #bettergrades thead, #bettergrades tfoot { background-color: lightgray; } #bettergrades th, #bettergrades td { padding: 0.5em; border: 1px solid; }");	
 });
