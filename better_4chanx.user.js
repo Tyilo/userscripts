c14a56a7e0ec3f982bb8eb3d4619ca2fd2d39bf8
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2013-07-05 16:25:27 +0200

    Add https and auto-resize images on opening a thread

--- better_4chanx.user.js
+++ better_4chanx.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @id             better4chanx@tyilo
 // @name           Better 4chan X
-// @version        1.1
+// @version        1.1.1
 // @namespace      http://tyilo.com/
 // @author         Tyilo
 // @description    
 // @include        http://boards.4chan.org/*
-// @include        http://sys.4chan.org/*
+// @include        https://boards.4chan.org/*
 // @run-at         document-start
 // @updateURL      https://raw.github.com/Tyilo/userscripts/master/better_4chanx.user.js
 // ==/UserScript==
@@ -14,6 +14,12 @@
 window.addEventListener('DOMContentLoaded', function(event)
 {
 	var unsafeWindow = window.unsafeWindow || window;
+	
+	// Auto-resize all images on opening a thread in a new tab
+	if(window.location.pathname.match(/^\/[^\/]*\/res\//))
+	{
+		document.querySelector('.expand-all-shortcut').click();
+	}
 	
 	// Better image resize:
 	document.querySelector('.opContainer + .replyContainer').style.clear = 'both';
