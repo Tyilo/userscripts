047172f1740f46b1546fde5f21f313cef1f62839
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2013-07-10 01:55:16 +0200

    Bump version

--- better_4chanx.user.js
+++ better_4chanx.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             better4chanx@tyilo
 // @name           Better 4chan X
-// @version        1.1.1
+// @version        1.1.2
 // @namespace      http://tyilo.com/
 // @author         Tyilo
 // @description    
