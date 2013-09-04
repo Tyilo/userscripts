b32b0019bf4dce9a0e224907f51053514640de38
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2013-09-04 10:46:25 +0200

    Update better_4chanx.user.js

--- better_4chanx.user.js
+++ better_4chanx.user.js
@@ -1,7 +1,7 @@
-// ==UserScript==
+ // ==UserScript==
 // @id             better4chanx@tyilo
 // @name           Better 4chan X
-// @version        1.1.3
+// @version        1.2
 // @namespace      http://tyilo.com/
 // @author         Tyilo
 // @description    
@@ -11,64 +11,156 @@
 // @updateURL      https://raw.github.com/Tyilo/userscripts/master/better_4chanx.user.js
 // ==/UserScript==
 
+var enableFeatures = {
+	autoresize: false,
+	preload_images: true,
+	newline_comment: true,
+	autoresize_scrolling: true,
+	thumbs_scrolling: true
+};
+
 window.addEventListener('DOMContentLoaded', function(event)
 {
 	var unsafeWindow = window.unsafeWindow || window;
 	
-	// Auto-resize all images on opening a thread in a new tab
-	if(window.location.pathname.match(/^\/[^\/]*\/res\//))
+	if(enableFeatures['autoresize'])
 	{
-		setTimeout(function check_expand() {
-			var el1 = document.querySelector('.expand-all-shortcut');
-			var el2 = document.querySelector('.contract-all-shortcut');
-			if(el1) {
-				el1.click();
-			} else if(!el2) {
-				setTimeout(check_expand, 100);
-			}
-		}, 0);
+		// Auto-resize all images on opening a thread in a new tab
+		if(window.location.pathname.match(/^\/[^\/]*\/res\//))
+		{
+			setTimeout(function check_expand() {
+				var el1 = document.querySelector('.expand-all-shortcut');
+				var el2 = document.querySelector('.contract-all-shortcut');
+				if(el1) {
+					el1.click();
+				} else if(!el2) {
+					setTimeout(check_expand, 100);
+				}
+			}, 0);
+		}
 	}
 	
-	// Better image resize:
-	document.querySelector('.opContainer + .replyContainer').style.clear = 'both';
+	if(enableFeatures['preload_images'])
+	{
+		var thumbs = document.querySelectorAll('a.fileThumb');
+		for(var i = 0; i < thumbs.length; i++)
+		{
+			var image = new Image();
+			image.src = thumbs[i].getAttribute('href');
+		}
+	}
+	
+	if(enableFeatures['newline_comment'])
+	{
+		// Better image resize:
+		document.querySelector('.opContainer + .replyContainer').style.clear = 'both';
+	}
+	
+	var mousePosition = [];
+	window.addEventListener('mousemove', function(event)
+	{
+		mousePosition = [event.x, event.y];
+	});
 	
 	// Better scrolling with space:
 	window.addEventListener('keydown', function(event)
 	{
-		if(event.keyCode === 32 && document.querySelector('.contract-all-shortcut') && !(event.target instanceof HTMLInputElement || event.target instanceof HTMLTextAreaElement))
+		var isExpanded = document.querySelector('.contract-all-shortcut');
+		if(event.keyCode === 32 && !(event.target instanceof HTMLInputElement || event.target instanceof HTMLTextAreaElement))
 		{
-			event.preventDefault();
-			var images = document.querySelectorAll('img.full-image');
-			var length = images.length;
-			var currentScroll = window.pageYOffset;
-			if(!event.shiftKey)
+			var reverse = event.shiftKey;
+			if(isExpanded)
 			{
-				for(var i = 0; i < length; i++)
+				if(!enableFeatures['autoresize_scrolling'])
 				{
-					if(images[i].y > currentScroll)
+					return;
+				}
+				event.preventDefault();
+				var images = document.querySelectorAll('img.full-image');
+				var length = images.length;
+				var currentScroll = window.pageYOffset;
+				if(!reverse)
+				{
+					for(var i = 0; i < length; i++)
 					{
-						unsafeWindow.scrollTo(0, images[i].y);
-						break;
+						if(images[i].y > currentScroll)
+						{
+							unsafeWindow.scrollTo(0, images[i].y);
+							break;
+						}
+					}
+					if(i === length) //At last image
+					{
+						unsafeWindow.scrollTo(0, 999999);
 					}
 				}
-				if(i === length) //At last image
+				else
 				{
-					unsafeWindow.scrollTo(0, 999999);
+					for(var i = length - 1; i >= 0; i--)
+					{
+						if(images[i].y < currentScroll)
+						{
+							unsafeWindow.scrollTo(0, images[i].y);
+							break;
+						}
+					}
+					if(i === -1) //At first image
+					{
+						unsafeWindow.scrollTo(0, 0);
+					}
 				}
 			}
 			else
 			{
-				for(var i = length - 1; i >= 0; i--)
+				if(!enableFeatures['thumbs_scrolling'])
 				{
-					if(images[i].y < currentScroll)
-					{
-						unsafeWindow.scrollTo(0, images[i].y);
-						break;
-					}
+					return;
 				}
-				if(i === -1) //At first image
+				var hover = document.elementFromPoint(mousePosition[0], mousePosition[1]);
+				console.log(mousePosition, hover);
+				if(hover instanceof HTMLImageElement)
 				{
-					unsafeWindow.scrollTo(0, 0);
+					event.preventDefault();
+					var images = document.querySelectorAll('img:not(.full-image)');
+					var length = images.length;
+					var index = -1;
+					if(!reverse)
+					{
+						for(var i = 0; i < length; i++)
+						{
+							if(images[i] === hover)
+							{
+								index = i + 1;
+								break;
+							}
+						}
+					}
+					else
+					{
+						for(var i = length - 1; i >= 0; i--)
+						{
+							if(images[i] === hover)
+							{
+								index = i - 1;
+								break;
+							}
+						}
+					}
+					if(index === length || index === -1)
+					{
+						if(!reverse)
+						{
+							unsafeWindow.scrollTo(0, 999999);
+						}
+						else
+						{
+							unsafeWdindow.scrollTo(0, 0);
+						}
+					}
+					else
+					{
+						unsafeWindow.scrollTo(0, images[index].y + images[index].height / 2 - mousePosition[1]);
+					}
 				}
 			}
 		}
